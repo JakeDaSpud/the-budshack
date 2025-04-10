bca1b1cf106eddbc3478e26d92690bcc6a42e1e9
Jake O'Reilly <78006039+JakeDaSpud@users.noreply.github.com>
2025-04-10 09:47:52 +0200

    :tada: started the budshack!!
    🧊 level shape flooring

---
+++ Data/config.gd
@@ -0,0 +1,6 @@
+extends Node
+
+var MOUSE_CAMERA_SENSITIVITY : float = 0.005;
+var CONTROLLER_CAMERA_SENSITIVITY : float = 0.05;
+
+var CAMERA_FOV : float = 60;
---
+++ Scenes/Entities/player.gd
@@ -0,0 +1,32 @@
+extends CharacterBody3D
+
+const _SPEED : float = 5;
+
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED;
+	$Camera3D.fov = CONFIG.CAMERA_FOV;
+
+func _physics_process(delta):
+	_handle_pause();
+	_handle_movement(delta);
+
+func _input(event):
+	if (event.get_class() == "InputEventMouseMotion"):
+		#print_debug("yeppp mouse");
+		rotate_y(-event.relative.x * CONFIG.MOUSE_CAMERA_SENSITIVITY);
+		$Camera3D.rotate_x(-event.relative.y * CONFIG.MOUSE_CAMERA_SENSITIVITY);
+
+func _handle_movement(delta : float):
+	var input_dir = Input.get_vector("Move_Left", "Move_Right", "Move_Forward", "Move_Back");
+	
+	if (input_dir != Vector2.ZERO):
+		var direction = $Camera3D.transform.FORWARD + Vector3(input_dir.x, 0, input_dir.y).normalized();
+		
+		velocity = direction * _SPEED;
+		
+		move_and_slide();
+
+func _handle_pause():
+	if (Input.is_action_pressed("Pause")):
+		print_debug("Quitting");
+		get_tree().quit();
